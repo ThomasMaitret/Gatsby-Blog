7821f36ee7c334d675bb9a16f89f38f90b733eba
Thomas Maitret <thomas.maitret@laposte.net>
2018-02-27 19:50:27 +0100

    removed unused assets + updated dependencies

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -1,18 +1,18 @@
-const colors = require("../styles/colors");
+const colors = require('../styles/colors');
 
 module.exports = {
-  pathPrefix: "/",
-  siteTitle: "Gatsby Blog",
-  siteLongTitle: "Gatsby StyledBlog Theme/Starter",
-  siteUrl: "https://thomasmaitret.github.io/portfolio/",
-  siteLanguage: "en",
-  siteLogo: "/logos/logo-1024.png",
-  siteDescription: "This is a starter/theme for GatsbyJS",
+  pathPrefix: '/',
+  siteTitle: 'Gatsby Blog',
+  siteLongTitle: 'Gatsby StyledBlog Theme/Starter',
+  siteUrl: 'https://thomasmaitret.github.io/portfolio/',
+  siteLanguage: 'en',
+  siteLogo: '/logos/logo-1024.png',
+  siteDescription: 'This is a starter/theme for GatsbyJS',
   // manifest.json
-  manifestName: "StyledBlog Gatsby starter",
-  manifestShortName: "StyledBlog",
-  manifestStartUrl: "/",
+  manifestName: 'StyledBlog Gatsby starter',
+  manifestShortName: 'StyledBlog',
+  manifestStartUrl: '/',
   manifestBackgroundColor: colors.first,
   manifestThemeColor: colors.firstLight,
-  manifestDisplay: "standalone"
+  manifestDisplay: 'standalone'
 };
